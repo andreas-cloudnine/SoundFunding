5c011158ecd1cf4df9adb08d4d57cfb720411e6d
Andreas Ylivainio <andreas@circuit.se>
2014-11-27 19:50:33 +0100

    Data stuff

---
+++ SoundFunding/Models/SoundFundingDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace SoundFunding.Models
+{
+    public class SoundFundingDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public SoundFundingDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static SoundFundingDbContext Create()
+        {
+            return new SoundFundingDbContext();
+        }
+
+        public DbSet<Cause> Causes { get; set; }
+    }
+}
