e70a0a6f1565aa8ace88c0928df4680c32a30c99
Andreas Ylivainio <andreas@circuit.se>
2014-11-27 18:51:09 +0100

    Initial commit with FB login

---
+++ SoundFunding/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SoundFunding.Startup))]
+namespace SoundFunding
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
