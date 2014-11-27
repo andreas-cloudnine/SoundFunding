4291b8d1a674f6cc1be4f3ad3025810929e01e07
debski <tomas.eld@circuit.se>
2014-11-27 21:25:41 +0100

    f

---
+++ SoundFunding/Controllers/AuthorizationController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+using SoundFunding.SpotifyClient;
+
+namespace SoundFunding.Controllers
+{
+    public class AuthorizationController : Controller
+    {
+        private const string RedirectUri = "http://soundfunding.azurewebsites.net/authorization/callback";
+
+        public ActionResult Login()
+        {
+            var url = "https://accounts.spotify.com/authorize?client_id={client_id}&response_type={response_type}&redirect_uri={redirect_uri}&scope={scope}";
+
+            url = url.Replace("{client_id}", SpotifyWebApiClient.ClientID);
+            url = url.Replace("{response_type}", "token");
+            url = url.Replace("{redirect_uri}", RedirectUri);
+            url = url.Replace("{scope}", string.Join(" ", SpotifyWebApiClient.AuthorizationScopes));
+
+            return Redirect(url);
+        }
+
+        public ActionResult Callback(string code, string state)
+        {
+            var token = new SpotifyWebApiClient(new BasicAuthenticator()).GetToken(code, RedirectUri);
+            SpotifyAuthenticator.AddTokenToSession(token);
+            return Content(token);
+        }
+    }
+}
--- SoundFunding/SpotifyClient/SpotifyWebApiClient.cs
+++ SoundFunding/SpotifyClient/SpotifyWebApiClient.cs
@@ -8,7 +8,7 @@ namespace SoundFunding.SpotifyClient
         internal const string ClientID = "28c9a4868be14e358a3256919946c7fd";
         internal const string ClientSecret = "2d687b1ebeba4fcbbdfde3031ab4ec01";
 
-        private static readonly IEnumerable<string> AuthorizationScopes = new[] {
+        internal static readonly IEnumerable<string> AuthorizationScopes = new[] {
                                                                                     "playlist-modify-public",
                                                                                     "user-read-private"
                                                                                 };
@@ -32,19 +32,13 @@ namespace SoundFunding.SpotifyClient
 
         public void Authorize(string redirectUri)
         {
-            var request = new RestRequest("https://accounts.spotify.com/authorize?client_id={client_id}&response_type={response_type}&redirect_uri={redirect_uri}&scope={scope}");
 
-            request.AddParameter("client_id", ClientID);
-            request.AddParameter("response_type", "token");
-            request.AddParameter("redirect_uri", redirectUri);
-            request.AddParameter("scope ", string.Join(" ", AuthorizationScopes));
-
-            Client.Execute(request);
         }
 
         public string GetToken(string code, string redirectUri)
         {
             var request = new RestRequest("https://accounts.spotify.com/api/token", Method.POST);
+            request.RequestFormat = DataFormat.Json;
 
             request.AddParameter("grant_type", "authorization_code", ParameterType.RequestBody);
             request.AddParameter("code", "code", ParameterType.RequestBody);
