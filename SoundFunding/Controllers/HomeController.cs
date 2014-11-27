12e69ae549a0739d78f709e4d403d7dc46994ef1
Andreas Ylivainio <andreas@circuit.se>
2014-11-27 21:08:15 +0100

    Pill

--- SoundFunding/Controllers/HomeController.cs
+++ SoundFunding/Controllers/HomeController.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity.Migrations;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using Microsoft.AspNet.Identity;
-using SoundFunding.Models;
+﻿using System.Web.Mvc;
 
 namespace SoundFunding.Controllers
 {
