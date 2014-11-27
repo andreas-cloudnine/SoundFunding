35c7c4f8a59a7dbfe95572181dfcec24fdaf12cf
debski <tomas.eld@circuit.se>
2014-11-27 21:39:57 +0100

    fgfg

--- SoundFunding/Controllers/AuthorizationController.cs
+++ SoundFunding/Controllers/AuthorizationController.cs
@@ -12,18 +12,20 @@ namespace SoundFunding.Controllers
             var url = "https://accounts.spotify.com/authorize?client_id={client_id}&response_type={response_type}&redirect_uri={redirect_uri}&scope={scope}";
 
             url = url.Replace("{client_id}", SpotifyWebApiClient.ClientID);
-            url = url.Replace("{response_type}", "token");
+            url = url.Replace("{response_type}", "code");
             url = url.Replace("{redirect_uri}", RedirectUri);
             url = url.Replace("{scope}", string.Join(" ", SpotifyWebApiClient.AuthorizationScopes));
 
             return Redirect(url);
         }
 
-        public ActionResult Callback(string code, string state)
+        public ActionResult Callback(string code, string state, string error)
         {
             var token = new SpotifyWebApiClient(new BasicAuthenticator()).GetToken(code, RedirectUri);
+            
             SpotifyAuthenticator.AddTokenToSession(token);
+
             return Content(token);
         }
-    }
+x    }
 }
