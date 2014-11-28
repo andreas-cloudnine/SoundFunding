80c930fb697851a72a4677ae623bc0ef4b577f77
debski <tomas.eld@circuit.se>
2014-11-28 14:07:01 +0100

    Seed stuff

--- SoundFunding/Migrations/Configuration.cs
+++ SoundFunding/Migrations/Configuration.cs
@@ -4,7 +4,7 @@ namespace SoundFunding.Migrations
 {
     using System.Data.Entity.Migrations;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<SoundFunding.Models.SoundFundingDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<SoundFundingDbContext>
     {
         public Configuration()
         {
@@ -20,8 +20,95 @@ namespace SoundFunding.Migrations
                 Name = "Rädda tigern",
                 ReceivingOrganization = "WWF",
                 GoalSum = 500,
-                Picture = "/html/img/bg-tiger-400f.png"
+                Picture = "/html/img/bg-tiger-400f.png",
+                SpotifyPlaylistUri = "spotify:user:sonymusicentertainment:playlist:2BkvZh4A1UVPQIps6ILKvd",
+                SpotifyUserAvatarUrl = "/html/img/andreas.jpg"
             });
+ 
+            db.Causes.Add(new Cause
+            {
+                Name = "Köp en flock getter – Actionaid",
+                ReceivingOrganization = "Actionaid",
+                GoalSum = 500,
+                Picture = "/html/img/get2.jpg",
+                SpotifyPlaylistUri = "spotify:user:sonymusicentertainment:playlist:2BkvZh4A1UVPQIps6ILKvd",
+                SpotifyUserAvatarUrl = "/html/img/andreas.jpg" 
+            });
+            db.Causes.Add(new Cause
+            {
+                Name = "Kvinnors rättigheter- Amnesty international",
+                ReceivingOrganization = "Amnesty international",
+                GoalSum = 500,
+                Picture = "/html/img/kvinnorsrätt2.jpg",
+                SpotifyPlaylistUri = "spotify:user:sonymusicentertainment:playlist:2BkvZh4A1UVPQIps6ILKvd",
+                SpotifyUserAvatarUrl = "/html/img/andreas.jpg" 
+            });
+            db.Causes.Add(new Cause
+            {
+                Name = "Stöd jourtelefonen för barn - Bris",
+                ReceivingOrganization = "Bris",
+                GoalSum = 500,
+                Picture = "/html/img/bris.jpg",
+                SpotifyPlaylistUri = "spotify:user:sonymusicentertainment:playlist:2BkvZh4A1UVPQIps6ILKvd",
+                SpotifyUserAvatarUrl = "/html/img/andreas.jpg"
+            });
+            db.Causes.Add(new Cause
+            {
+                Name = "Kampen mot ebola - läkare utan gränser",
+                ReceivingOrganization = "Läkare utan gränser",
+                GoalSum = 500,
+                Picture = "/html/img/ebola2.jpg",
+                SpotifyPlaylistUri = "spotify:user:sonymusicentertainment:playlist:2BkvZh4A1UVPQIps6ILKvd",
+                SpotifyUserAvatarUrl = "/html/img/andreas.jpg"  
+            });
+            db.Causes.Add(new Cause
+            {
+                Name = "Hjälp Stockholms hemlösa - Stadsmissionen",
+                ReceivingOrganization = "Stadsmissionen",
+                GoalSum = 500,
+                Picture = "/html/img/hemlös.jpg",
+                SpotifyPlaylistUri = "spotify:user:sonymusicentertainment:playlist:2BkvZh4A1UVPQIps6ILKvd",
+                SpotifyUserAvatarUrl = "/html/img/andreas.jpg"   
+            });
+            db.Causes.Add(new Cause 
+            {
+                Name = "Stoppa vapenhandeln – Amnesty International",
+                ReceivingOrganization = "Amnesty International",
+                GoalSum = 500,
+                Picture = "/html/img/noguns.jpg",
+                SpotifyPlaylistUri = "spotify:user:sonymusicentertainment:playlist:2BkvZh4A1UVPQIps6ILKvd",
+                SpotifyUserAvatarUrl = "/html/img/andreas.jpg" 
+            });
+            db.Causes.Add(new Cause
+            {
+                Name = "Plantera träd - Vi-skogen",
+                ReceivingOrganization = "WWF",
+                GoalSum = 500,
+                Picture = "/html/img/tree.jpg"   ,
+                SpotifyPlaylistUri = "spotify:user:sonymusicentertainment:playlist:2BkvZh4A1UVPQIps6ILKvd",
+                SpotifyUserAvatarUrl = "/html/img/andreas.jpg"
+            });
+            db.Causes.Add(new Cause
+            {
+                Name = "Stoppa valfisket - WWF",
+                ReceivingOrganization = "WWF",
+                GoalSum = 500,
+                Picture = "/html/img/whale.jpg",
+                SpotifyPlaylistUri = "spotify:user:sonymusicentertainment:playlist:2BkvZh4A1UVPQIps6ILKvd",
+                SpotifyUserAvatarUrl = "/html/img/andreas.jpg"
+            }); 
+            db.Causes.Add(new Cause
+            {
+                Name = "Vaccinering av barn -unicef",
+                ReceivingOrganization = "Unicef",
+                GoalSum = 500,
+                Picture = "/html/img/vaccin2.jpg",                 
+                SpotifyPlaylistUri = "spotify:user:sonymusicentertainment:playlist:2BkvZh4A1UVPQIps6ILKvd",
+                SpotifyUserAvatarUrl = "/html/img/andreas.jpg"
+            });
+
+            db.SaveChanges();
+
         }
     }
 }
